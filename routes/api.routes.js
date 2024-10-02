f5c336f074aae788e9ebd8227b55a018f845df1b
juan olarte <juan.olartex64@gmail.com>
2024-10-01 21:09:32 -0500

    primer commit

---
+++ routes/api.routes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const user = require('./user.routes')
+const product = require ('./product.routes')
+const reserve = require ('./reserve.routes')
+const event = require ('./event.routes')
+//crea la ruta de la api
+router.use('/api', product )
+router.use('/api', reserve)
+router.use('/api', event)
+router.use('/api', user)
+
+
+module.exports = router
